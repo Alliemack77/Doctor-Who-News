b59e098e01dbf5ba9a7545e5391b256416f2bda7
Allison Leggett <alliemack77@yahoo.com>
2022-04-11 13:52:26 -0700

    added client directory

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const cors = require('cors')
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 const url = "https://www.bbc.co.uk/blogs/doctorwho"
 
 const app = express()
